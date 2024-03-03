21fdc628e33bf523963a423fa8d6de5b78f194aa
Simon Kaley <skaley@csu.fullerton.edu>
2024-03-02 21:58:48 -0800

    added database with vercel

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/asteroids/:name', async (req, res) => {
     try {
@@ -109,6 +110,11 @@ app.delete('/api/asteroids', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+// Catch-all route to serve 'index.html' for non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
